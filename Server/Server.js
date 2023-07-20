016dc3d3465fddb623bb01807ede2a36e30829f6
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-07-20 18:55:11 +0200

    Build 1.0.1

--- Server/Server.js
+++ Server/Server.js
@@ -1,11 +1,8 @@
 const express = require(`express`)
 const cors = require(`cors`)
 const path = require('path');
-// const mongoose = require(`mongoose`)
-// const webRoutes = require(`./Server_src/Routers/WebDataRoutes`);
-// const dataBaseRoutes = require(`./Server_src/Routers/DataBaseRoutes`);
-const webRoutes = require(`./Routes/WebRoutes`);
 
+const webRoutes = require(`./Routes/WebRoutes`);
 
 const app = express();
 
@@ -16,18 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../Client/build')));
 
 
-
 app.use(`/api/webData/`, webRoutes);
-// app.use(`/api/dataBase/`, dataBaseRoutes);
-
-
-
-// mongoose.connect(`mongodb+srv://gym_booking:gym_booking@cluster0.pv477hr.mongodb.net/Stocks?retryWrites=true&w=majority`, {});
-// const db = mongoose.connection;
-
-// db.once('open', () => console.log('Connected to the database'));
-// db.on('error', err => console.log('Error ' + err));
-
 
 
 const server = app.listen(process.env.PORT || 8000, () => {
