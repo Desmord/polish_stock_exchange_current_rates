3a924b6324b7ea6dcd71bd29a2c067f799735475
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-07-24 12:29:52 +0200

    HomePage topography change

--- Client/src/Components/Stocks/Stocks.tsx
+++ Client/src/Components/Stocks/Stocks.tsx
@@ -11,37 +11,79 @@ const Stocks = ({ stocks }: { stocks: StockInterface[] }) => {
     return (
         <div className={styles.container}>
             <div className={styles.stocks}>WIG 20 STOCKS</div>
-            {stocks.map((stockIndex: StockInterface, arrayIndex: number) => {
+            <div className={styles.stocksContainer}>
 
-                if (stockIndex.error) {
-                    return (
-                        <div className={styles.index} key={arrayIndex}>
-                            <span>Network error</span>
-                        </div>
-                    )
-                }
+                <div>
 
-                return (
-                    <div className={styles.index} key={arrayIndex}>
-                        <span>{stockIndex.companyShortcut}</span>
-                        <span>{stockIndex.mainValue}</span>
-                        <span
-                            className={`
+
+                    {stocks.slice(0, Math.ceil(stocks.length / 2)).map((stockIndex: StockInterface, arrayIndex: number) => {
+
+                        if (stockIndex.error) {
+                            return (
+                                <div className={styles.index} key={arrayIndex}>
+                                    <span>Network error</span>
+                                </div>
+                            )
+                        }
+
+                        return (
+                            <div className={styles.index} key={arrayIndex}>
+                                <span>{stockIndex.companyShortcut}</span>
+                                <span>{stockIndex.mainValue}</span>
+                                <span
+                                    className={`
                                 ${isLoss(stockIndex.percentageChange) ? styles.loss : ``}
                                 ${isProfit(stockIndex.percentageChange) ? styles.profit : ``}
                             `}>
-                            {stockIndex.percentageChange}%
-                        </span>
-                        <span
-                            className={`
+                                    {stockIndex.percentageChange}%
+                                </span>
+                                <span
+                                    className={`
                                 ${isLoss(stockIndex.valueChange) ? styles.loss : ``}
                                 ${isProfit(stockIndex.valueChange) ? styles.profit : ``}
                             `}>
-                            {stockIndex.valueChange}
-                        </span>
-                    </div>
-                )
-            })}
+                                    {stockIndex.valueChange}
+                                </span>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <div>
+                    {stocks.slice(Math.ceil(stocks.length / 2)).map((stockIndex: StockInterface, arrayIndex: number) => {
+
+                        if (stockIndex.error) {
+                            return (
+                                <div className={styles.index} key={arrayIndex}>
+                                    <span>Network error</span>
+                                </div>
+                            )
+                        }
+
+                        return (
+                            <div className={styles.index} key={arrayIndex}>
+                                <span>{stockIndex.companyShortcut}</span>
+                                <span>{stockIndex.mainValue}</span>
+                                <span
+                                    className={`
+                                ${isLoss(stockIndex.percentageChange) ? styles.loss : ``}
+                                ${isProfit(stockIndex.percentageChange) ? styles.profit : ``}
+                            `}>
+                                    {stockIndex.percentageChange}%
+                                </span>
+                                <span
+                                    className={`
+                                ${isLoss(stockIndex.valueChange) ? styles.loss : ``}
+                                ${isProfit(stockIndex.valueChange) ? styles.profit : ``}
+                            `}>
+                                    {stockIndex.valueChange}
+                                </span>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
         </div>
     )
 }
--- Client/src/Utils/URLs.ts
+++ Client/src/Utils/URLs.ts
@@ -1,3 +1,2 @@
 export const GET_STOCK_URL = "/api/webData/getCompanyStockValue/";
 export const GET_WIG_URL = "/api/webData/getWigValue/";
-
